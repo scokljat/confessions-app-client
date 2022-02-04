1dc066806278f19d1e0f97fb301fefca27872bef
scokljat <90629541+scokljat@users.noreply.github.com>
2022-02-04 17:38:04 +0100

    Improves redux

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import thunk from "redux-thunk";
 import reducers from "./reducers";
 import App from "./App";
 import "./index.css";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <Provider store={store}>
     <App />
